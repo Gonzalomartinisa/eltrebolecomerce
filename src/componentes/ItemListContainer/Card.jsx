8658b941ecff236be860bb4c643b8e81f6e745a8
Gonzalomartinisa <gonzalomartionnisa@gmail.com>
2022-04-21 23:47:56 -0300

    ItemListContainert

--- src/componentes/ItemListContainer/Card.jsx
+++ src/componentes/ItemListContainer/Card.jsx
@@ -12,7 +12,7 @@ const Card = ({
 
   return (
     <div className="card" style={{width:"18rem"}}>
-  <img src={imagen} className="card-img-top" alt="..."/>
+  <img src={"..."} className="card-img-top" alt="..."/>
   <div className="card-body">
     <h5 className="card-title">{texto}</h5>
     <p className="card-text">{greeting}</p>
